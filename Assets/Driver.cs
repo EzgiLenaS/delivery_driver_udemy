43c81d6b10102dc4c5ef2e5a394fd8d3c2aaf6e0
EzgiLenaS <ezgilena00@gmail.com>
2025-03-13 07:14:51 +0300

    riding car is on control by user with keyboard. Assests are added. Next is Simple follow camera.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 0.1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,11 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // from -1 to 1 for each frame
+        // Time.deltaTime makes the frame rate independent
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
