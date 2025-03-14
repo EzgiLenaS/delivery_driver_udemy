39f35455820c8f238f1d034be01308dcc5f75c88
EzgiLenaS <ezgilena00@gmail.com>
2025-03-14 08:32:05 +0300

    When a package is taken then on the scene it should not be seen anymore. Therefore, Destroy() is written and now the package will not on the scene or anywhere else anymore. Next is using GetComponent and affect the color of the car when it picks up a package until give it to the customer.

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 5f;
+
     bool hasPackage = false;
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -10,10 +12,12 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Package")
+        // "!hasPackage" is for 1 package at a time
+        if(collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            Destroy(collision.gameObject, destroyDelay);
         }
 
         if(collision.tag == "Customer" && hasPackage)
